069723c705143cb1aadb769cfc8a5e1dcf9f6c54
Patrick Matthews <matthewssrpat@comcast.net>
2017-07-17 12:33:14 -0500

    Add files via upload
    Working 8 channel DMX Tester with 8 sliders, 8 momentary buttons to activate the sliders and 8 leds to indicate which slider has been selected with channel.  Issue is with storage and retrieval of EEPROM data stored and retrieved with put and get.  Al other functionality works, working on multiple channels simultaneously next.

--- DMX_Master_I2C_Final_V3-b.ino
+++ DMX_Master_I2C_Final_V3-b.ino
@@ -10,6 +10,7 @@
 #include <Adafruit_RGBLCDShield.h>
 #include <utility/Adafruit_MCP23017.h>
 
+#define MAX_CHANNEL 8
 #define MAX_CHANNELS 512
 #define DMX_MASTER_CHANNELS MAX_CHANNELS
 // The shield uses the I2C SCL and SDA pins. On classic Arduinos
@@ -38,8 +39,7 @@ int LCD_C=16;  // lcd columns
 //create manual override variables
 boolean SetAddress = false;
 boolean SetIntensity = false;
-
-int DMX_MASTER_ADDRESS = 1;
+int DMX_MASTER_ADDRESS;
 int DMX_ADDRESS = 1;
 int SelectVal = 0;
 
@@ -48,38 +48,28 @@ typedef struct {
 int versionCheck;   // Just a number to check to ensure we've read in valid settings!  
 int channel;        // channel address.
 } addressVals_t;
-addressVals_t ADDRESS[5];
 
-int button_address[6] = {5,6,7,8,9,10};
- // slider pin assignments
- // slider1 = A29
- // slider2 = A30
- // slider3 = A31
- // slider4 = A32
- // slider5 = A33
- // slider6 = A34
-int slider_address[6] = {1,2,3,6,7,8};
-// button state variable for all buttons true for ON false for OFF
-int button_state[6] = {LOW,LOW,LOW,LOW,LOW,LOW};
-int old_button_state1= LOW;
-int old_button_state2= LOW;
-int old_button_state3= LOW;
-int old_button_state4= LOW;
-int old_button_state5= LOW;
-int old_button_state6= LOW;
-int button_State1 = LOW;
-int button_State2 = LOW;
-int button_State3 = LOW;
-int button_State4 = LOW;
-int button_State5 = LOW;
-int button_State6 = LOW;
+addressVals_t ADDRESS[MAX_CHANNEL];
 
-// boolean button_State = false;
-int EEPROM_ADD[1]={1};
-int EEPROM_address = 0;
+int EEPROM_ADD[MAX_CHANNEL] = {0,0,0,0,0,0,0,0};
+// button pin assignments digital pins
+int button_address[MAX_CHANNEL] = {5,6,7,8,9,10,11,12};   // digital pins
+ // slider pin assignments analog pins
+int slider_address[MAX_CHANNEL] = {1,2,3,6,7,8,9,10};   // analopg pins
+// button state variable for all buttons HIGH for OFF LOW for ON
+int button_state[MAX_CHANNEL] = {LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW};
+int old_button_state[MAX_CHANNEL] = {HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
+
+int button_State1 = HIGH;
+int button_State2 = HIGH;
+int button_State3 = HIGH;
+int button_State4 = HIGH;
+int button_State5 = HIGH;
+int button_State6 = HIGH;
+int button_State7 = HIGH;
+int button_State8 = HIGH;
 
 byte overmenu = 0;
-int overpercent = 0;
 // button read variables for lcd shield
 uint8_t i=0;
 uint8_t buttons =0;
@@ -96,26 +86,32 @@ DMX_Master dmx_master ( MAX_CHANNELS , 2 );
 
 //---------------------------------------PIN configuration
 const int StatusLed = 13;
-const int BUTTON1 = 5;
+const int BUTTON1 = 5;    // digital D5
 const int BUTTON2 = 6;
 const int BUTTON3 = 7;
 const int BUTTON4 = 8;
 const int BUTTON5 = 9;
 const int BUTTON6 = 10;
+const int BUTTON7 = 11;
+const int BUTTON8 = 12;
 
-const int SLIDER1 = 1;
-const int SLIDER2 = 2;
-const int SLIDER3 = 3;
-const int SLIDER4 = 6;
-const int SLIDER5 = 7;
-const int SLIDER6 = 8;
+const int SLIDER1 = 0;    // analog A1
+const int SLIDER2 = 1;
+const int SLIDER3 = 2;
+const int SLIDER4 = 3;
+const int SLIDER5 = A6;
+const int SLIDER6 = A7;
+const int SLIDER7 = A8;
+const int SLIDER8 = A9;
 
-const int LED1 = 23;
+const int LED1 = 23;      // digital D23
 const int LED2 = 24;
-const int LED3 = 25;    // A0
-const int LED4 = 26;    // A1
-const int LED5 = 27;    // A2
-const int LED6 = 28;    // A3
+const int LED3 = 25;    // 
+const int LED4 = 26;    // 
+const int LED5 = 27;    // 
+const int LED6 = 28;    // 
+const int LED7 = 29;
+const int LED8 = 30;
 
 // A4 and A5 reserved for SCL and SDA i2c bus
 const int break_usec = 200;
@@ -183,52 +179,60 @@ void cleanScreen()
 }
 // -----------------------------------------
 // read settingsfrom eeprom
-void readSettings() {
+void readSettings() 
+{
 int EEPROM_address = 0;
-for (byte i = 0; i < 5; i++) {
-EEPROM.get(EEPROM_address, ADDRESS);
-EEPROM_ADD[i] = EEPROM_address;
-if (ADDRESS[i].versionCheck != 9898) {
-// Set default values for the settings and save them to EEPROM
-ADDRESS[i].versionCheck = 9898;
-switch (i) {
-case 0:
-ADDRESS[0].channel = 3;  // default setting
-break;
-case 1:
-ADDRESS[1].channel = 13;  // default setting
-break;
-case 2:
-ADDRESS[2].channel = 23;  // default setting
-case 3:
-ADDRESS[3].channel = 33;  // default setting
-break;
-case 4:
-ADDRESS[4].channel = 43;  // default setting
-break;
-//case 5:
-//ADDRESS[5].channel = 53;  // default setting
-//break;
-break;
-} // end switch
-EEPROM.put(EEPROM_ADD[i], ADDRESS[i]);
-} // end if
-} // end for
-}
-// end read settings
-//------------------------------------------
+for (byte i = 0; i < MAX_CHANNEL; i++) 
+  {
+    EEPROM.get(EEPROM_address, ADDRESS);
+    EEPROM_ADD[i] = EEPROM_address;
+    if (ADDRESS[i].versionCheck != 1234) 
+      {
+      // Set default values for the settings and save them to EEPROM
+      ADDRESS[i].versionCheck = 1234;
+      switch (i) 
+        {
+        case 0:
+          ADDRESS[0].channel = 3;  // default setting
+        break;
+        case 1:
+          ADDRESS[1].channel = 13;  // default setting
+        break;
+        case 2:
+          ADDRESS[2].channel = 23;  // default setting
+        break;
+        case 3:
+          ADDRESS[3].channel = 33;  // default setting
+        break;
+        case 4:
+          ADDRESS[4].channel = 43;  // default setting
+        break;
+        case 5:
+          ADDRESS[5].channel = 53;  // default setting
+        break;
+        case 6:
+          ADDRESS[6].channel = 63;  // default setting
+        break;
+        case 7:
+          ADDRESS[7].channel = 73;  // default setting
+        break;
+        break;
+        } // end switch
+        EEPROM.put(EEPROM_ADD[i], ADDRESS[i]);
+      } // end if
+  } // end for
 
-//============================================
+}  // end read settings
+//------------------------------------------
 // splash screen
 void SplashScreen(void){
   lcd.setCursor(0,0);
   lcd.print("PMA DMX Tester  ");
-  delay(5000);
   lcd.setCursor(0,1);
-  lcd.print("Controller by GG");
-  delay(1000);
+  lcd.print("Controller      ");
+  delay(3000);
   lcd.setCursor(0,1);
-  lcd.print("SET CHANNEL     ");
+  lcd.print("SELCT CHANNEL   ");
   delay(1000);
   lcd.clear(); 
   btnCurrIteration = btnMaxIteration;
@@ -242,14 +246,13 @@ void DoIntensity(int j)
             switch(j) {
             case 0:
             {
-              int EEPROM_address = (j * sizeof(addressVals_t));
-              EEPROM.put(EEPROM_address, ADDRESS[j]);
               // send value to channel
               while (button_state[j] == LOW) 
                 {
                 digitalWrite(LED1,HIGH);
                 lcd.setCursor(0,0);
                 lcd.print("CHANNEL:");
+                lcd.setCursor(0,1);
                 dtostrf(ADDRESS[j].channel, 3, 0, buffer); // Leave room for too large numbers!
                 lcd.setCursor(9,0);
                 lcd.print( buffer);  
@@ -261,16 +264,14 @@ void DoIntensity(int j)
                 digitalWrite(StatusLed,HIGH);  
                 dmx_master.setChannelValue ( ADDRESS[j].channel, sensorValue );
                 uint8_t buttons = lcd.readButtons();
-                if(buttons & BUTTON_RIGHT) { button_state[j] = HIGH;DoAddress();}
-                if(buttons & BUTTON_LEFT) { button_state[j] = HIGH;DoAddress ();}
+                if(buttons & BUTTON_RIGHT) { button_state[j] = HIGH;digitalWrite(LED1,LOW);DoAddress();}
+                if(buttons & BUTTON_LEFT) { button_state[j] = HIGH;digitalWrite(LED1,LOW);DoAddress ();}
                 }
                 digitalWrite(LED1,LOW);
             break;
             }
             case 1:
             {
-              int EEPROM_address = (j * sizeof(addressVals_t));
-              EEPROM.put(EEPROM_address, ADDRESS[j]);
               // send value to channel
               while (button_state[j] == LOW) 
                 {
@@ -296,8 +297,6 @@ void DoIntensity(int j)
             }
           case 2:
           {
-              int EEPROM_address = (j * sizeof(addressVals_t));
-              EEPROM.put(EEPROM_address, ADDRESS[j]);
               // send value to channel
               while (button_state[j] == LOW) 
                 {
@@ -323,8 +322,6 @@ void DoIntensity(int j)
           }
           case 3:
           {
-              int EEPROM_address = (j * sizeof(addressVals_t));
-              EEPROM.put(EEPROM_address, ADDRESS[j]);
               // send value to channel
               while (button_state[j] == LOW) 
                 {
@@ -350,8 +347,6 @@ void DoIntensity(int j)
           }
           case 4:
           {
-              int EEPROM_address = (j * sizeof(addressVals_t));
-              EEPROM.put(EEPROM_address, ADDRESS[j]);
               // send value to channel
               while (button_state[j] == LOW) 
                 {
@@ -377,8 +372,6 @@ void DoIntensity(int j)
           }
           case 5:
           {
-              //int EEPROM_address = (j * sizeof(addressVals_t));
-              //EEPROM.put(EEPROM_address, ADDRESS[j]);
               // send value to channel
               while (button_state[j] == LOW) 
                 {
@@ -402,6 +395,57 @@ void DoIntensity(int j)
                 digitalWrite(LED6,LOW);
           break;
           }
+        case 6:
+          {
+              // send value to channel
+              while (button_state[j] == LOW) 
+                {
+                digitalWrite(LED7,HIGH);
+                lcd.setCursor(0,0);
+                lcd.print("CHANNEL:");
+                dtostrf(ADDRESS[j].channel, 3, 0, buffer); // Leave room for too large numbers!
+                lcd.setCursor(9,0);
+                lcd.print( buffer);  
+                int sensor_Value = analogRead(SLIDER7);
+                int sensorValue = sensor_Value/4;
+                dtostrf(sensorValue, 3, 0, buffer); // Leave room for too large numbers!              
+                lcd.setCursor(0,1);
+                lcd.print( buffer);
+                digitalWrite(StatusLed,HIGH);  
+                dmx_master.setChannelValue ( ADDRESS[j].channel, sensorValue );
+                uint8_t buttons = lcd.readButtons();
+                if(buttons & BUTTON_RIGHT) { button_state[j] = HIGH;DoAddress();}
+                if(buttons & BUTTON_LEFT) { button_state[j] = HIGH;DoAddress ();}
+                }
+                digitalWrite(LED7,LOW);
+          break;
+          }
+       case 7:
+          {
+              // send value to channel
+              while (button_state[j] == LOW) 
+                {
+                digitalWrite(LED8,HIGH);
+                lcd.setCursor(0,0);
+                lcd.print("CHANNEL:");
+                dtostrf(ADDRESS[j].channel, 3, 0, buffer); // Leave room for too large numbers!
+                lcd.setCursor(9,0);
+                lcd.print( buffer);  
+                int sensor_Value = analogRead(SLIDER8);
+                int sensorValue = sensor_Value/4;
+                dtostrf(sensorValue, 3, 0, buffer); // Leave room for too large numbers!              
+                lcd.setCursor(0,1);
+                lcd.print( buffer);
+                digitalWrite(StatusLed,HIGH);  
+                dmx_master.setChannelValue ( ADDRESS[j].channel, sensorValue );
+                uint8_t buttons = lcd.readButtons();
+                if(buttons & BUTTON_RIGHT) { button_state[j] = HIGH;DoAddress();}
+                if(buttons & BUTTON_LEFT) { button_state[j] = HIGH;DoAddress ();}
+                }
+                digitalWrite(LED8,LOW);
+          break;
+          }
+        
           break;
           }// end switch
    } // end DoIntensity
@@ -410,115 +454,167 @@ void DoAddress(void)
   {
   char buffer[16];
   uint8_t buttons = lcd.readButtons();
-  lcd.setCursor(0,0);
-  lcd.print("CHANNEL:");
-  lcd.setCursor(0,1);
-  lcd.print("Select Slider");
-  dtostrf(DMX_MASTER_ADDRESS, 3, 0, buffer); // Leave room for too large numbers!
-  lcd.setCursor(9,0);
-  lcd.print( buffer);  
-  //Set current Address
-  // showe current address
-  dtostrf(DMX_MASTER_ADDRESS, 3, 0, buffer); // Leave room for too large numbers!
-  lcd.setCursor(9,0);
-  lcd.print( buffer); 
-  old_button_state1 = button_State1;
-  button_State1 = digitalRead(BUTTON1);
+    lcd.setCursor(0,0);
+    lcd.print("CHANNEL:");
+    lcd.setCursor(0,1);
+    lcd.print("SELECT SLIDER");
+    dtostrf(DMX_MASTER_ADDRESS, 3, 0, buffer); // Leave room for too large numbers!
+    lcd.setCursor(9,0);
+    lcd.print( buffer); 
+    old_button_state[0] = button_State1;
+    button_State1 = digitalRead(BUTTON1);
   if (button_State1 == LOW)
      {
      SelectVal = 0;
+     int EEPROM_address = (SelectVal * sizeof(addressVals_t));
+     EEPROM.put(EEPROM_address, ADDRESS[SelectVal]);
      button_state[SelectVal] = button_State1;
-     if(button_State1 != old_button_state1)
+     if(button_State1 != old_button_state[SelectVal])
      {
        button_state[SelectVal] = button_State1;
      } else {
-        button_state[SelectVal] = old_button_state1;
+        button_state[SelectVal] = old_button_state[SelectVal];
      }
      ADDRESS[SelectVal].channel = DMX_MASTER_ADDRESS;
      cleanScreen();
      DoIntensity(SelectVal);              
      }// end button_state1
-   button_State2 = digitalRead(BUTTON2);
+     old_button_state[1] = button_State2;
+     button_State2 = digitalRead(BUTTON2);
    if (button_State2 == LOW)
      {
      SelectVal = 1;
+     int EEPROM_address = (SelectVal * sizeof(addressVals_t));
+     EEPROM.put(EEPROM_address, ADDRESS[SelectVal]);
      button_state[SelectVal] = button_State2;
-     if(button_State2 != old_button_state2)
+     if(button_State2 != old_button_state[SelectVal])
      {
-       button_state[SelectVal] = button_State2;
+       button_state[SelectVal] = old_button_state[SelectVal];
      } else {
-        button_state[SelectVal] = old_button_state2;
+        button_state[SelectVal] = old_button_state[SelectVal];
      }
      ADDRESS[SelectVal].channel = DMX_MASTER_ADDRESS;
      cleanScreen();
      DoIntensity(SelectVal);              
      }// end button_state2
-  button_State3 = digitalRead(BUTTON3);
+    old_button_state[2] = button_State3;
+    button_State3 = digitalRead(BUTTON3);
   if (button_State3 == LOW)
      {
      SelectVal = 2;
+     int EEPROM_address = (SelectVal * sizeof(addressVals_t));
+     EEPROM.put(EEPROM_address, ADDRESS[SelectVal]);
      button_state[SelectVal] = button_State3;
-  if(button_State3 != old_button_state3)
+  if(button_State3 != old_button_state[SelectVal])
      {
        button_state[SelectVal] = button_State3;
      } else {
-        button_state[SelectVal] = old_button_state3;
+        button_state[SelectVal] = old_button_state[SelectVal];
      }
      ADDRESS[SelectVal].channel = DMX_MASTER_ADDRESS;
      cleanScreen();
      DoIntensity(SelectVal);              
      }// end button_state3
+   old_button_state[3] = button_State4;
+   button_State4 = digitalRead(BUTTON4);
   if (button_State4 == LOW)
      {
-     SelectVal = 3;
+      SelectVal = 3;
+     int EEPROM_address = (SelectVal * sizeof(addressVals_t));
+     EEPROM.put(EEPROM_address, ADDRESS[SelectVal]);
      button_state[SelectVal] = button_State4;
-  button_State4 = digitalRead(BUTTON4);
-  if(button_State4 != old_button_state4)
+     button_State4 = digitalRead(BUTTON4);
+  if(button_State4 != old_button_state[SelectVal])
      {
        button_state[SelectVal] = button_State4;
      } else {
-        button_state[SelectVal] = old_button_state4;
+        button_state[SelectVal] = old_button_state[SelectVal];
      }
      ADDRESS[SelectVal].channel = DMX_MASTER_ADDRESS;
      cleanScreen();
      DoIntensity(SelectVal);              
      }// end button_state4
+     old_button_state[4] = button_State5;
+     button_State5 = digitalRead(BUTTON5);
   if (button_State5 == LOW)
      {
-     SelectVal = 4;
+      SelectVal = 4;
+     int EEPROM_address = (SelectVal * sizeof(addressVals_t));
+     EEPROM.put(EEPROM_address, ADDRESS[SelectVal]);
      button_state[SelectVal] = button_State5;
-   button_State5 = digitalRead(BUTTON5);
-  if(button_State5 != old_button_state5)
+     button_State5 = digitalRead(BUTTON5);
+  if(button_State5 != old_button_state[SelectVal])
      {
        button_state[SelectVal] = button_State5;
      } else {
-        button_state[SelectVal] = old_button_state5;
+        button_state[SelectVal] = old_button_state[SelectVal];
      }
      ADDRESS[SelectVal].channel = DMX_MASTER_ADDRESS;
      cleanScreen();
      DoIntensity(SelectVal);              
      }// end button_state5
-   button_State6 = digitalRead(BUTTON6);
+     old_button_state[5] = button_State6;
+     button_State6 = digitalRead(BUTTON6);
   if (button_State6 == LOW)
      {
      SelectVal = 5;
+     int EEPROM_address = (SelectVal * sizeof(addressVals_t));
+     EEPROM.put(EEPROM_address, ADDRESS[SelectVal]);
      button_state[SelectVal] = button_State6;
-     if(button_State6 != old_button_state6)
+     if(button_State6 != old_button_state[SelectVal])
      {
        button_state[SelectVal] = button_State6;
      } else {
-        button_state[SelectVal] = old_button_state1;
+        button_state[SelectVal] = old_button_state[SelectVal];
      }
      ADDRESS[SelectVal].channel = DMX_MASTER_ADDRESS;
      cleanScreen();
      DoIntensity(SelectVal);              
      }// end button_state6
+     old_button_state[6] = button_State7;
+     button_State7 = digitalRead(BUTTON7);
+  if (button_State7 == LOW)
+     {
+     SelectVal = 6;
+     int EEPROM_address = (SelectVal * sizeof(addressVals_t));
+     EEPROM.put(EEPROM_address, ADDRESS[SelectVal]);
+     button_state[SelectVal] = button_State7;
+     if(button_State7 != old_button_state[SelectVal])
+     {
+       button_state[SelectVal] = button_State7;
+     } else {
+        button_state[SelectVal] = old_button_state[SelectVal];
+     }
+     ADDRESS[SelectVal].channel = DMX_MASTER_ADDRESS;
+     cleanScreen();
+     DoIntensity(SelectVal);              
+     }// end button_state7  
+     old_button_state[7] = button_State8;
+     button_State8 = digitalRead(BUTTON8);
+  if (button_State8 == LOW)
+     {
+     SelectVal = 7;
+     int EEPROM_address = (SelectVal * sizeof(addressVals_t));
+     EEPROM.put(EEPROM_address, ADDRESS[SelectVal]);
+     button_state[SelectVal] = button_State8;
+     if(button_State8 != old_button_state[SelectVal])
+     {
+       button_state[SelectVal] = button_State8;
+     } else {
+        button_state[SelectVal] = old_button_state[SelectVal];
+     }
+     ADDRESS[SelectVal].channel = DMX_MASTER_ADDRESS;
+     cleanScreen();
+     DoIntensity(SelectVal);              
+     }// end button_state8
+       
      if(buttons & BUTTON_RIGHT)
        {
        if(DMX_MASTER_ADDRESS < 513)
           {
           DMX_MASTER_ADDRESS++;
           DMX_ADDRESS = DMX_MASTER_ADDRESS;
+
 //        button_State1 = digitalRead(BUTTON1);
           } // end if < 513
       }// end if BUTTON_RIGHT
@@ -539,7 +635,7 @@ void setup()
   PrintBegin();
   // read defult / EEPROM settings and update to current
   readSettings();
- 
+  DMX_MASTER_ADDRESS = ADDRESS[0].channel;
   // enable DMX master interface
   dmx_master.enable ();
   // Set channel 1 - 512 @ 100%
@@ -558,13 +654,24 @@ void setup()
   digitalWrite ( BUTTON5, HIGH );
   pinMode ( BUTTON6, INPUT_PULLUP );
   digitalWrite ( BUTTON6, HIGH );
+  pinMode ( BUTTON7, INPUT_PULLUP );
+  digitalWrite ( BUTTON7, HIGH );
+  pinMode ( BUTTON8, INPUT_PULLUP );
+  digitalWrite ( BUTTON8, HIGH );
+
   pinMode ( LED1, OUTPUT );
   pinMode ( LED2, OUTPUT );
   pinMode ( LED3, OUTPUT );
   pinMode ( LED4, OUTPUT );
   pinMode ( LED5, OUTPUT );
   pinMode ( LED6, OUTPUT );
+  pinMode ( LED7, OUTPUT );
+  pinMode ( LED8, OUTPUT );
 
+  //pinMode (SLIDER5, INPUT );
+  //pinMode (SLIDER6, INPUT );
+  //pinMode (SLIDER7, INPUT );
+  //pinMode (SLIDER8, INPUT );
 
   SplashScreen();
   }
